3df647bd5a4df258d6207bbdb817837f33ba4b5b
Joshua M. Clulow <josh@sysmgr.org>
2020-09-01 11:55:27 -0700

    grafana v7.1.5

---
+++ grafana/build.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+function info {
+	printf 'INFO: %s\n' "$*"
+}
+
+function header {
+	printf -- '\n'
+	printf -- '----------------------------------------------------------\n'
+	printf -- 'INFO: %s\n' "$*"
+	printf -- '----------------------------------------------------------\n'
+	printf -- '\n'
+}
+
+function fatal {
+	printf 'ERROR: %s\n' "$*" >&2
+	exit 1
+}
+
+function extract_to {
+	local name="$1"
+	local tar="$2"
+	local outdir="$3"
+	local extra="$4"
+
+	stamp="$outdir/.extracted"
+	if [[ ! -f "$stamp" ]]; then
+		rm -rf "$outdir"
+		info "extracting $name..."
+		mkdir -p "$outdir"
+		gtar -C "$outdir" $extra -xzf "$tar"
+		info "extracted $name ok"
+		touch "$stamp"
+	else
+		info "$name extracted already"
+	fi
+
+	return 0
+}
+
+function download_to {
+	local name="$1"
+	local url="$2"
+	local path="$3"
+
+	if [[ ! -f "$path" ]]; then
+		info "downloading $name..."
+		if ! curl -fL -o "$path" "$url"; then
+			fatal "$name download failed"
+			exit 1
+		fi
+		info "ok"
+	else
+		info "downloaded $name already"
+	fi
+
+	return 0
+}
+
+ROOT=$(cd "$(dirname "$0")" && pwd)
+export GOPATH="$ROOT/cache/gopath"
+export GOCACHE="$ROOT/cache/gocache"
+export YARN_CACHE_FOLDER="$ROOT/cache/yarncache"
+YARNROOT="$ROOT/cache/yarnroot"
+
+ARTEFACT="$ROOT/artefact"
+mkdir -p "$ARTEFACT"
+WORK="$ROOT/work"
+mkdir -p "$WORK"
+
+#
+# Directory for workaround scripts that we can put in PATH:
+#
+WORKAROUND="$ROOT/cache/workaround"
+rm -rf "$WORKAROUND"
+mkdir -p "$WORKAROUND"
+
+VER='7.1.5'
+URL="https://github.com/grafana/grafana/archive/v${VER}.tar.gz"
+
+GOVER='1.14.6'
+GOURL="https://illumos.org/downloads/go$GOVER.illumos-amd64.tar.gz"
+
+YARNVER='1.22.5'
+YARNURL="https://github.com/yarnpkg/yarn/releases/download/v$YARNVER/yarn-v$YARNVER.tar.gz"
+
+#
+# Download artefacts to use during build:
+#
+header 'downloading artefacts'
+
+yarnfile="$ARTEFACT/yarn-v$YARNVER.tar.gz"
+download_to yarn "$YARNURL" "$yarnfile"
+
+gofile="$ARTEFACT/go1.14.6.illumos-amd64.tar.gz"
+download_to go "$GOURL" "$gofile"
+
+file="$ARTEFACT/grafana-v$VER.tar.gz"
+download_to grafana "$URL" "$file"
+
+#
+# Extract artefacts:
+#
+header 'extracting artefacts'
+
+extract_to yarn "$yarnfile" "$YARNROOT" --strip-components=1
+
+export GOROOT="$ROOT/cache/go$GOVER"
+extract_to go "$gofile" "$GOROOT"
+
+extract_to grafana "$file" "$WORK" --strip-components=1
+
+#
+# Create workaround wrappers:
+#
+header 'preparing workaround wrappers'
+
+cat >"$WORKAROUND/python2" <<'EOF'
+#!/usr/bin/bash
+exec /usr/bin/python2.7 "$@"
+EOF
+chmod 0755 "$WORKAROUND/python2"
+
+cat >"$WORKAROUND/make" <<'EOF'
+#!/usr/bin/bash
+exec gmake "$@"
+EOF
+chmod 0755 "$WORKAROUND/make"
+
+cat >"$WORKAROUND/cc" <<'EOF'
+#!/usr/bin/bash
+exec gcc "$@"
+EOF
+chmod 0755 "$WORKAROUND/cc"
+
+#
+# Build Grafana:
+#
+header 'building grafana'
+
+export PATH="$GOPATH/bin:$GOROOT/bin:$YARNROOT/bin:$WORKAROUND:$PATH"
+
+cd "$WORK"
+
+info 'expurgating...'
+#
+# The "cypress" browser testing system does not build on illumos systems,
+# but is also not actually a required part of the package.
+#
+rm -rf "$WORK/packages/grafana-e2e"
+
+header 'yarn install...'
+yarn install --frozen-lockfile
+
+header 'build go binaries...'
+go run build.go -cc=gcc -skipRpm -skipDeb build
+
+header 'build frontend files (this may take a while)...'
+go run build.go -cc=gcc -skipRpm -skipDeb build-frontend
+
+header 'build package archive...'
+go run build.go -cc=gcc -skipRpm -skipDeb pkg-archive
+go run build.go -cc=gcc -skipRpm -skipDeb sha-dist
