e2aeb9032c4ebfa4ddaebf985071e86dabc96f52
Joshua M. Clulow <jmc@oxide.computer>
2021-11-21 20:34:30 -0800

    hidapi 0.11.0

---
+++ hidapi/build.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+ROOT=$(cd "$(dirname "$0")" && pwd)
+. "$ROOT/../lib/common.sh"
+
+ARTEFACT="$ROOT/artefact"
+mkdir -p "$ARTEFACT"
+WORK="$ROOT/work"
+mkdir -p "$WORK"
+SRC32="$WORK/src32"
+mkdir -p "$SRC32"
+SRC64="$WORK/src64"
+mkdir -p "$SRC64"
+PROTO="$WORK/proto"
+mkdir -p "$PROTO"
+
+#
+# Directory for workaround scripts that we can put in PATH:
+#
+WORKAROUND="$ROOT/cache/workaround"
+rm -rf "$WORKAROUND"
+mkdir -p "$WORKAROUND"
+
+NAM='hidapi'
+VER='0.11.0'
+URL="https://github.com/libusb/$NAM/archive/refs/tags/$NAM-$VER.tar.gz"
+
+if [[ -x /usr/gcc/10/bin/gcc ]]; then
+	GCC_DIR=/usr/gcc/10/bin
+elif [[ -x /opt/gcc-10/bin/gcc ]]; then
+	GCC_DIR=/opt/gcc-10/bin
+else
+	fatal "Could not find GCC in any expected location"
+fi
+info "using $GCC_DIR/gcc: $($GCC_DIR/gcc --version | head -1)"
+info "using $GCC_DIR/g++: $($GCC_DIR/g++ --version | head -1)"
+
+#
+# Download artefacts to use during build:
+#
+header 'downloading artefacts'
+
+file="$ARTEFACT/$NAM-$VER.tar.gz"
+download_to "$NAM" "$URL" "$file"
+
+#
+# Extract artefacts:
+#
+header 'extracting artefacts'
+
+extract_to "$NAM" "$file" "$SRC32" --strip-components=1
+extract_to "$NAM" "$file" "$SRC64" --strip-components=1
+
+header "building $NAM"
+
+export PATH="$WORKAROUND:$PATH"
+
+cd "$SRC32"
+
+mkdir build
+cd build
+PKG_CONFIG_PATH=/usr/lib/pkgconfig \
+    CFLAGS='-m32' \
+    cmake \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_INSTALL_LIBDIR=lib \
+    ..
+
+info "make..."
+gmake -j8
+
+info "make install..."
+gmake install DESTDIR="$PROTO"
+
+cd "$SRC64"
+
+mkdir build
+cd build
+PKG_CONFIG_PATH=/usr/lib/pkgconfig \
+    CFLAGS='-m64' \
+    cmake \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_INSTALL_LIBDIR=lib/amd64 \
+    ..
+
+info "make..."
+gmake -j8
+
+info "make install..."
+gmake install DESTDIR="$PROTO"
+
+if [[ -z "$OUTPUT_TYPE" ]]; then
+	OUTPUT_TYPE=ips
+fi
+
+case "$OUTPUT_TYPE" in
+ips)
+	make_package "library/$NAM" \
+	    'a library for communicating with USB and Bluetooth HID devices' \
+	    "$WORK/proto"
+	header 'build output:'
+	pkgrepo -s "$WORK/repo" list
+	pkgrecv -a -d "$WORK/$NAM-$VER.p5p" -s "$WORK/repo" "$NAM@$VER-1.0"
+	ls -lh "$WORK/$NAM-$VER.p5p"
+	exit 0
+	;;
+none)
+	#
+	# Just leave the build tree as-is without doing any more work.
+	#
+	exit 0
+	;;
+*)
+	fatal "unknown output type: $OUTPUT_TYPE"
+	;;
+esac
