d762111ccc3548b643eda72ad0b5fc5237c41926
David Pacheco <dap@oxidecomputer.com>
2022-08-19 14:02:59 -0700

    just use a local patch instead of pinning to some other commit

--- cockroach/build.sh
+++ cockroach/build.sh
@@ -23,15 +23,8 @@ WORKAROUND="$ROOT/cache/workaround"
 rm -rf "$WORKAROUND"
 mkdir -p "$WORKAROUND"
 
-# We are currently pinned to a commit on the CockroachDB v22.1 release branch.
-# This commit is somewhat after 22.1.5.  It apparently just missed the cutoff
-# for 22.1.6.  We're doing this because we need cockroachdb/cockroach#86425 in
-# order to build.  We should be able to switch directly to v22.1.7 when that is
-# released.
-VER='22.1.5-oxide'
-COCKROACHDB_CLONE_REF="release-22.1"
-COCKROACHDB_CHECKOUT_REF="d2f8b61d0af382d86e688b45a09ecfcbb8abb785"
-COCKROACHDB_CHECKOUT_REF="310ef7155cc5fe2a9d68d3c826d645da18c5cad8"
+VER='22.1.5'
+COCKROACHDB_CLONE_REF="v$VER"
 
 GOVER='1.17.11'
 SYSGOVER=$( (pkg info go-117 || true) | awk '/Version:/ { print $NF }')
@@ -66,11 +59,12 @@ if [[ ! -f "$stamp" ]]; then
 	repo=https://github.com/cockroachdb/cockroach
 	info "cloning $repo branch $COCKROACHDB_CLONE_REF ..."
 	mkdir -p "$ROOT/cache/gopath/src/github.com/cockroachdb"
-	git clone --branch $COCKROACHDB_CLONE_REF --single-branch $repo \
+	git clone \
+	    --recurse-submodules \
+	    --branch $COCKROACHDB_CLONE_REF \
+	    --depth 1 \
+	    $repo \
 	    cache/gopath/src/github.com/cockroachdb/cockroach
-	(cd cache/gopath/src/github.com/cockroachdb/cockroach &&
-	    git checkout $COCKROACHDB_CHECKOUT_REF &&
-	    git submodule update --init)
 	touch "$stamp"
 else
 	info 'already cloned'
