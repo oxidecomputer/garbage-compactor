154017f12fa792bd17d250cd8f62fb83a4e571e5
Adam Suczewski <adamlouis@users.noreply.github.com>
2026-04-17 14:19:51 -0700

    gitup

--- mfg/build.sh
+++ mfg/build.sh
@@ -2,6 +2,15 @@
 #
 # Copyright 2026 Oxide Computer Company
 #
+# `mfg` is a package for the manufacturing software deployed to station
+# computers.  It uses require dependencies to ensure the required tools are
+# installed, and incorporate
+# dependencies to lock against particular versions of those tools.  To date,
+# manufacturing station tools have been managed via a combination of
+# confomat-oxide and ad-hoc pkg installs, leading to version drift between
+# stations.  This package ensures the correct versions are installed and
+# updated in lock step.
+#
 
 set -o errexit
 set -o pipefail
@@ -11,7 +20,7 @@ ROOT=$(cd "$(dirname "$0")" && pwd)
 
 NAM='mfg'
 CREV=0
-VER="1.0.$CREV"
+VER="0.1.$CREV"
 
 WORK="$ROOT/work"
 mkdir -p "$WORK"
@@ -28,7 +37,7 @@ ips)
 	pkgrepo create "$WORK/repo"
 	pkgrepo add-publisher -s "$WORK/repo" helios-dev
 	make_package_simple "oxide/$NAM" \
-	    'Manufacturing software meta-package' \
+	    'Manufacturing software package' \
 	    "$WORK/proto" \
 	    "$ROOT/mfg.p5m" \
 	    "$VER"
